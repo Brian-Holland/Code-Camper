d9d3ba803c5ff9de85ec2f1b68c7b8bdc02df68f
Brian <brian.holland.pr@gmail.com>
2020-06-01 23:05:08 -0700

    added mongoose and db setup

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
+
+//Connect to db
+connectDB();
 
 //initialize express
 const app = express();
@@ -14,9 +18,16 @@ const PORT = process.env.PORT || 5000;
 //mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
     )
 );
+
+//handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //close server & exit process
+    server.close(() => process.exit(1));
+});
