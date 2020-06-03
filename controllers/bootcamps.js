e1f795aa117c0f449b1d7d45295de1e626b60089
Brian <brian.holland.pr@gmail.com>
2020-06-02 22:58:37 -0700

    added update and delete route logic

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,11 @@ exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamps = await Bootcamp.find();
 
-        res.status(200).json({ success: true, data: bootcamps });
+        res.status(200).json({
+            success: true,
+            numberResults: bootcamps.length,
+            data: bootcamps,
+        });
     } catch (err) {
         res.status(400).json({ success: false });
     }
@@ -49,13 +53,43 @@ exports.createBootcamp = async (req, res, next) => {
 //@desc     Update bootcamp
 //@route    PUT /api/v1/bootcamps/:id
 //access    Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Update single bootcamp" });
+exports.updateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(200).json({ success: false });
+    }
 };
 
 //@desc     Delete bootcamp
 //@route    DELETE /api/v1/bootcamps/:id
 //access    Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Delete single bootcamp" });
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: `Deleted ${bootcamp.name}`,
+        });
+    } catch (err) {
+        res.status(200).json({ success: false });
+    }
 };
